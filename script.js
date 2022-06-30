41933a474d10a486f5078c1cce441e4b5545b91c
brandontaylor156 <brandon.david4@outlook.com>
2022-06-30 09:11:52 -0700

    removed math.floor from hours

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ function getSecondsSinceStartOfDay() {
       
   setInterval( function() {
       var time = getSecondsSinceStartOfDay();
-      console.log(time);
+      
       seconds = Math.floor(time % 60);
       minutes = Math.floor(time / 60 % 60);
-      hours = Math.floor(time / 3600 % 12);
-     
+      hours = time / 3600 % 12;
+    
       document.querySelector('#seconds').style.transform = "rotate(" + seconds*6 + "deg)";
       document.querySelector('#minutes').style.transform = "rotate(" + minutes*6 + "deg)";
       document.querySelector('#hour').style.transform = "rotate(" + hours*30 + "deg)";
